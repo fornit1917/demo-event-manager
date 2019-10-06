f9855385e4b47fadf7d0987b74f463645c231262
Vit S <fornit1917@gmail.com>
2019-10-07 01:23:18 +0300

    fix

--- ApiApp/Services/EventsService.cs
+++ ApiApp/Services/EventsService.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using EventManager.ApiApp.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EventManager.ApiApp.Services {
     public class EventsService : IEventsService {
+        private static Expression<Func<Event, Event>> Projection = (Event x) => new Event {
+            Id = x.Id,
+            Name = x.Name,
+            Place = x.Place,
+            Type = x.Type,
+            EventDate = x.EventDate,
+            MaxGuests = x.MaxGuests,
+            IsArchived = x.IsArchived,
+            InvitedGuestsCount = x.Guests.Count,
+        };
+
         private AppDbContext _db;
 
         public EventsService(AppDbContext db) {
@@ -16,16 +28,7 @@ namespace EventManager.ApiApp.Services {
         public Task<Event[]> GetEvents() {
             return _db.Events
                 .Where(x => x.IsArchived == false)
-                .Select(x => new Event {
-                    Id = x.Id,
-                    Name = x.Name,
-                    Place = x.Place,
-                    Type = x.Type,
-                    EventDate = x.EventDate,
-                    MaxGuests = x.MaxGuests,
-                    IsArchived = x.IsArchived,
-                    InvitedGuestsCount = x.Guests.Count,
-                })
+                .Select(Projection)
                 .AsNoTracking()
                 .ToArrayAsync();
         }
@@ -48,16 +51,8 @@ namespace EventManager.ApiApp.Services {
         public Task<Event> GetEvent(int eventId) {
             return _db.Events
                 .Where(x => x.Id == eventId)
-                .Select(x => new Event {
-                    Id = x.Id,
-                    Name = x.Name,
-                    Place = x.Place,
-                    Type = x.Type,
-                    EventDate = x.EventDate,
-                    MaxGuests = x.MaxGuests,
-                    IsArchived = x.IsArchived,
-                    InvitedGuestsCount = x.Guests.Count,
-                }).FirstOrDefaultAsync();
+                .Select(Projection)
+                .FirstOrDefaultAsync();
         }
     }
 }
